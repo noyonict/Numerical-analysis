326c8e3701f7440f432cddbdaf5d3d78691871cc
Noyon <noyonmassive@gmail.com>
2016-11-05 14:33:03 +0600

    Add files via upload
    Update not hard coded file

--- GaussianElimination.java
+++ GaussianElimination.java
@@ -1,4 +1,4 @@
-package numerical.analysis;
+package gaussianelimination;
 
 /**
  *
@@ -11,24 +11,26 @@ public class GaussianElimination {
         int array[][] = {{1, 3, -5, 2}, {3, 11, -9, 4}, {-1, 1, 6, 3}};
         System.out.println("Initial value: ");
         ge.display_matrix(array);
-
+        int arrayLength = array.length;
+        System.out.println(arrayLength + " ");
         int firstZero = array[1][0];
         int secondZero = array[2][0];
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= array.length; i++) {
             array[1][i] = array[1][i] - ((array[0][i] / array[0][0]) * firstZero);
             array[2][i] = array[2][i] - ((array[0][i] / array[0][0]) * secondZero);
+            if (i >= arrayLength) {
+                System.out.println("\nFirst step: ");
+                ge.display_matrix(array);
+                int thirdZero = array[2][1];
+                for (int k = 1; k < array[arrayLength - 1].length; k++) {
+                    array[2][k] = array[2][k] - ((array[1][k] / array[1][1]) * thirdZero);
+                }
+                System.out.println("\nSecond step: ");
+                ge.display_matrix(array);
+            }
         }
+
         
-        System.out.println("\nFirst step: ");
-        ge.display_matrix(array);
-        
-        int thirdZero = array[2][1];
-        for (int i = 1; i < 4; i++) {
-            array[2][i] = array[2][i] - ((array[1][i] / array[1][1]) * thirdZero);
-        }
-        
-        System.out.println("\nSecond step: ");
-        ge.display_matrix(array);
 
     }
 
